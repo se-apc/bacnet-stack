03c16fe4464b74668fe559bec41a93ce93803956
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2023-08-22 06:35:47 -0700

    Adding handy few-liners for building and running router-ipv6 application.

---
+++ build.router.ipv6.sh
@@ -0,0 +1,1 @@
+make BBMD=server BACDL=bip6 -s -C apps router-ipv6
---
+++ router-ipv6.sh
@@ -0,0 +1,12 @@
+
+export BACNET_IFACE=eth0
+export BACNET_BIP6_IFACE=eth1
+
+#Also uses these configurations, but defaults to these values if not set:
+export BACNET_IP_PORT=47808
+export BACNET_BIP6_PORT=47808
+export BACNET_BIP6_BROADCAST=FF0E
+export BACNET_IP_NET=1
+export BACNET_IP6_NET=2
+
+apps/router-ipv6/bacroute
