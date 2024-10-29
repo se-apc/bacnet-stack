c3c3720659d5f19fbe44bf4f3f3bafcf324efc0e
Steve Karg <skarg@users.sourceforge.net>
2024-10-29 13:17:39 -0500

    Added WriteProperty setter for BACnet Unsigned Integer values. (#834)

--- src/bacnet/wp.h
+++ src/bacnet/wp.h
@@ -46,6 +46,15 @@ typedef struct BACnet_Write_Property_Data {
  */
 typedef bool (*write_property_function)(BACNET_WRITE_PROPERTY_DATA *wp_data);
 
+/**
+ * @brief API for setting a BACnet Unsigned Integer property value
+ * @param object_instance [in] Object instance number
+ * @param value [in] New value to set
+ * @return true if successful, else false
+ */
+typedef bool (*bacnet_property_unsigned_setter)(
+    uint32_t object_instance, BACNET_UNSIGNED_INTEGER value);
+
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
@@ -88,6 +97,11 @@ bool write_property_empty_string_valid(
 
 BACNET_STACK_EXPORT
 bool write_property_bacnet_array_valid(BACNET_WRITE_PROPERTY_DATA *wp_data);
+bool write_property_unsigned_decode(
+    BACNET_WRITE_PROPERTY_DATA *wp_data,
+    BACNET_APPLICATION_DATA_VALUE *value,
+    bacnet_property_unsigned_setter setter,
+    BACNET_UNSIGNED_INTEGER maximum);
 
 #ifdef __cplusplus
 }
