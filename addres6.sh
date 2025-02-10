62e032764bb64eaacbe802a079a9da3488f992d5
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2025-02-10 14:37:13 +0000

    Adding bacnet bbmd like sending the address-resolution req every 1[s]

---
+++ addres6.sh
@@ -0,0 +1,19 @@
+
+# export BACNET_IFACE=eth0
+# export BACNET_BIP6_IFACE=eth1
+export BACNET_IFACE=ens37
+export BACNET_BIP6_IFACE=ens33
+
+#Also uses these configurations, but defaults to these values if not set:
+export BACNET_IP_PORT=47808
+export BACNET_BIP6_PORT=47809
+export BACNET_BIP6_BROADCAST=FF02
+export BACNET_IP_NET=1
+export BACNET_IP6_NET=2
+
+export BACNET_BBMD_PORT=47809
+export TARGET_VMAC=13
+export TARGET_IP6=2001:bb6:c2ab:6c58:2a29:86ff:fe2f:6eef
+export TARGET_PORT=47808
+
+apps/addr-res/addres 55
