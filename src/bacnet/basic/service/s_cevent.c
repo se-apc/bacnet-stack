d6078016f4bc5a50e82d74b31f976ce2605e3601
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2025-06-18 11:26:55 +0100

    Adding possibility of sending confirmed event notification to a device that had not been bound

--- src/bacnet/basic/service/s_cevent.c
+++ src/bacnet/basic/service/s_cevent.c
@@ -101,6 +101,9 @@ uint8_t Send_CEvent_Notify(
 
     /* is the device bound? */
     status = address_get_by_device(device_id, &max_apdu, &dest);
+    fprintf(stderr, "[%s %d %s]: DID = %d; status = '%s'\r\n",
+            __FILE__, __LINE__, __func__,
+            device_id, status ? "true" : "false");
     if (status) {
         if (sizeof(Handler_Transmit_Buffer) < max_apdu) {
             max_apdu = sizeof(Handler_Transmit_Buffer);
@@ -111,3 +114,23 @@ uint8_t Send_CEvent_Notify(
 
     return invoke_id;
 }
+
+/** Sends an Confirmed Alarm/Event Notification regardless whether the recipient's device is bound.
+ * @ingroup EVNOTFCN
+ *
+ * @param data [in] The information about the Event to be sent.
+ * @param dest [in] BACNET_ADDRESS of the destination device
+ * @return invoke id of outgoing message, or 0 if communication is disabled,
+ *         or no tsm slot is available.
+ */
+uint8_t Send_CEvent_Notify_Dest_Addr(
+    const BACNET_EVENT_NOTIFICATION_DATA *data, BACNET_ADDRESS *dest)
+{
+    const unsigned max_apdu = sizeof(Handler_Transmit_Buffer);
+    uint8_t invoke_id =
+        Send_CEvent_Notify_Address(
+                Handler_Transmit_Buffer, max_apdu, data, dest);
+
+    return invoke_id;
+}
+
